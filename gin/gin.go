c568ab83f57163f409734d2a98a5f1128abbbdf1
조석규 <ezaurum@gmail.com>
2018-04-22 22:30:04 +0900

    Add gin handler

---
+++ gin/gin.go
@@ -0,0 +1,50 @@
+package render
+
+import (
+	"github.com/ezaurum/cthulthu/render/boongeoppang"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/render"
+)
+
+//check implementation
+var _ render.HTMLRender = Render{}
+
+func Default() (Render) {
+	return New(boongeoppang.DefaultTemplateDir)
+}
+
+func NewDebug(templateDir string, engine *gin.Engine) {
+	b, c := boongeoppang.LoadDebug(templateDir)
+	engine.HTMLRender = Render{
+		templateContainer: b,
+	}
+	go func() {
+		tc := <-c
+		engine.HTMLRender = Render{
+			templateContainer: tc,
+		}
+	}()
+}
+
+// New instance
+func New(templateDir string) Render {
+	return Render{
+		templateContainer: boongeoppang.Load(templateDir),
+	}
+}
+
+type Render struct {
+	templateContainer *boongeoppang.TemplateContainer
+}
+
+// Instance find by name
+func (r Render) Instance(name string, data interface{}) render.Render {
+	layout, isExist := r.templateContainer.Get(name)
+	if !isExist {
+		panic("not exist template " + name)
+	}
+	return render.HTML{
+		Template: layout.Layout,
+		Data:     data,
+	}
+}
